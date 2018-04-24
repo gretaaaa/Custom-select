06de47773cee649e0bbb435273a97bf98095b212
Greta Ermacora <greta.ermacora.ge@gmail.com>
2018-04-24 16:27:34 +0200

    rename variables

--- js/javascriptSelectCustom.js
+++ js/javascriptSelectCustom.js
@@ -5,72 +5,72 @@
 		arraySelect.each(function(){
 			var select = $(this);
 			var label = select.find("label");
-			var lista = select.find("ul");
-			var elementoLista = select.find("li");
+			var list = select.find("ul");
+			var listElement = select.find("li");
 
-			//salvo il primo elemnto della lista nella label
-			var primoElemento = (elementoLista.first()).text();
-			label.html(primoElemento);
+			//save the first element of the list into the label
+			var firstElement = (listElement.first()).text();
+			label.html(firstElement);
 
-			//seleziono il primo elemento della lista
-			var liSelezionato = elementoLista.first();
-			liSelezionato.addClass("active");
+			//select the first element of the list
+			var selectedLi = listElement.first();
+			selectedLi.addClass("active");
 
 			label.click(function(){
-				//nascondo o visualizzo la lista
-				lista.toggle();
-				//aggiungo la classe active all'ultimo elemento della lista selezionato
-				liSelezionato.addClass("active");
+				//hide or show the list
+				list.toggle();
+				//add class 'active' at the last selected element of the list
+				selectedLi.addClass("active");
 			});
 
-			elementoLista.click(function(){
-				//nascondo la lista
-				lista.hide();
-				//salvo nella label il testo dell'elemento selezionato
-				var elementoScelto = $(this);
-				label.html(elementoScelto.text());
+			listElement.click(function(){
+				//hide the list
+				list.hide();
+				//save text of the last selected element into the list
+				var elementChosen = $(this);
+				label.html(elementChosen.text());
 			});
 
-			//seleziono il primo elemento della lista
-			var liSelezionato = elementoLista.first();
-			liSelezionato.addClass("active");
+			//select the first element of the list
+			var selectedLi = listElement.first();
+			selectedLi.addClass("active");
 
-			//calcolo l'indice dell'elemento in ultima posizione
-			indiceUltimoElemento = (elementoLista.last()).index();
+			//calculate index of the element in last position
+			indexLastElement = (listElement.last()).index();
 		
 						
-			elementoLista.on({
+			listElement.on({
 				mouseenter: function(){
-					elementoLista.removeClass("active");
-					liSelezionato = $("li:hover");
-					elementoLista.removeClass("noHover");
+					listElement.removeClass("active");
+					selectedLi = $("li:hover");
+					listElement.removeClass("noHover");
 				},
 				mouseleave: function(){
-					liSelezionato.addClass("active");
+					selectedLi.addClass("active");
 				}
 			});
 
 
 			$(document).on("keydown", function(event){
-				if(lista.is(':visible')){
-					if(event.which == 40 && (liSelezionato.index() != indiceUltimoElemento)){
-						elementoLista.addClass("noHover");
-						liSelezionato.removeClass("active");
-						liSelezionato = liSelezionato.next();
-						liSelezionato.addClass("active");
-					} else if(event.which == 38 && (liSelezionato.index() != 0)){
-						elementoLista.addClass("noHover");
-						liSelezionato.removeClass("active");
-						liSelezionato = liSelezionato.prev();
-						liSelezionato.addClass("active");
+				if(list.is(':visible')){
+					if(event.which == 40 && (selectedLi.index() != indexLastElement)){
+						listElement.addClass("noHover");
+						selectedLi.removeClass("active");
+						selectedLi = selectedLi.next();
+						selectedLi.addClass("active");
+					} else if(event.which == 38 && (selectedLi.index() != 0)){
+						listElement.addClass("noHover");
+						selectedLi.removeClass("active");
+						selectedLi = selectedLi.prev();
+						selectedLi.addClass("active");
 					} else if(event.which == 13){
-						//nascondo la lista
-						lista.hide();
-						label.html(liSelezionato.text());
+						//hide the list
+						list.hide();
+						label.html(selectedLi.text());
 					}
 				} else {
 					if(event.which == 13 && select.is(':focus')){
-						lista.show();
+						list.show();
 					}
 				}
 			});
